81f07df418e098e38e5d2ba65521e4f7211f7e27
andrew wilson <andrew.j.wilson7@gmail.com>
2023-06-05 14:14:30 +0100

    moved rsvp up

--- src/App.js
+++ src/App.js
@@ -15,11 +15,17 @@ function App() {
           <div class="container-fluid py-5">
             <h1 class="display-4 fw-bold">JOSIE & ANDI</h1>
             <h2 class="fs-4 text-center">Join us to celebrate <br /> 10 years of marriage <br />
-            16th july 2023</h2>
+              16th july 2023</h2>
           </div>
         </div>
 
         <div class="row align-items-md-stretch">
+          <div class="col-md-4">
+            <div class="h-100 d-flex fs-4 bg-light rounded-5">
+              <button class="btn btn-primary btn-lg btn-block text-white flex-fill rounded-5" type="button" role="link" onClick={() => openInNewTab("https://forms.gle/9Eg8c6L9dhqs9Y868")}><h2>RSVP</h2></button>
+            </div>
+          </div>
+
           <div class="col-md-4">
             <div class="h-100 p-5 text-white bg-secondary rounded-5">
               <h2>2pm 'till late</h2>
@@ -30,21 +36,17 @@ function App() {
           <div class="col-md-4">
             <div class="h-100 p-5 fs-4 bg-light rounded-5">
               <p>
-                Pizza 🍕 <br/> 
-                Look fab! <br/>
-                Kids OK (9pm curfew) <br/>
-                No Gifts ... <br/>
-                Donations welcome: <br/>
-                <a href="https://beyond-bars.org/">Donate to Books Beyond Bars</a> <br/>
+                Pizza 🍕 <br />
+                Look fab! <br />
+                Kids OK (9pm curfew) <br />
+                No Gifts ... <br />
+                Donations welcome: <br />
+                <a href="https://beyond-bars.org/">Donate to Books Beyond Bars</a> <br />
                 <a href="https://www.sparkle.org.uk/">Donate to Sparkle</a>
               </p>
             </div>
           </div>
-          <div class="col-md-4">
-            <div class="h-100 d-flex fs-4 bg-light rounded-5">
-            <button class="btn btn-primary btn-lg btn-block text-white flex-fill rounded-5" type="button" role="link" onClick={() => openInNewTab("https://forms.gle/9Eg8c6L9dhqs9Y868")}><h2>RSVP</h2></button>
-            </div>
-          </div>
+
         </div>
       </div>
     </main>
